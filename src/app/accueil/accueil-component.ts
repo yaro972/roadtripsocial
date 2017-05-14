515d49d7054aece1feba31d0909bdefc66792779
Thierry Aronoff <thierry.aronoff@gmail.com>
2017-05-14 21:49:03 +0200

    Mise en place menu + image d'entête

---
+++ src/app/accueil/accueil-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-accueil',
+  templateUrl: './accueil-component.html',
+  styleUrls: ['./accueil-component.css']
+})
+export class Accueil  { 
+  
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { Accueil } from './accueil/accueil-component';
+
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ 
+    AppComponent,
+    Accueil
+     ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
