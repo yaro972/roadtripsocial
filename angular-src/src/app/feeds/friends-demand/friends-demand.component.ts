a6d5400bf7f8d02b630436a999a19701aaa0edda
Thierry Aronoff <thierry.aronoff@gmail.com>
2017-09-01 21:25:02 +0200

    Ajout lien de détails d'un ami

--- angular-src/src/app/feeds/friends-demand/friends-demand.component.ts
+++ angular-src/src/app/feeds/friends-demand/friends-demand.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Subscription } from 'rxjs/Subscription';
 
-import { JsonPipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 import { AuthService } from './../../services/auth/auth.service';
 import { ShowImagePipe } from './../../show-images/pipes/show-image.pipe';
@@ -28,7 +28,8 @@ export class FriendsDemandComponent implements OnInit, OnDestroy {
   waitingfriendListDemands: Array<any> = [];
 
   constructor(
-    private _authService: AuthService
+    private _authService: AuthService,
+    private _router: Router
   ) { }
 
   ngOnInit() {
@@ -54,6 +55,20 @@ export class FriendsDemandComponent implements OnInit, OnDestroy {
     });
   }
 
+  onShowDetail(index) {
+    const selectedUserDemand = this.waitingfriendListDemands[index];
+    let userId = '';
+
+    if (selectedUserDemand.friendsList[0]._id !== this.ownId) {
+      userId = selectedUserDemand.friendsList[0]._id;
+    }
+    if (selectedUserDemand.friendsList[1]._id !== this.ownId) {
+      userId = selectedUserDemand.friendsList[1]._id;
+    }
+
+    this._router.navigate(['/details-membres', userId]);
+
+  }
   onClickAccept(index) {
     this.subAcceptDemand =
       this._authService
