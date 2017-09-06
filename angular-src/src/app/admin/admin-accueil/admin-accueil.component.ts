47d0ad3e799a3b882d69c6ddf72820c1ea589184
Thierry Aronoff <thierry.aronoff@gmail.com>
2017-09-06 17:09:29 +0200

    Fix problème build

--- angular-src/src/app/admin/admin-accueil/admin-accueil.component.ts
+++ angular-src/src/app/admin/admin-accueil/admin-accueil.component.ts
@@ -9,20 +9,32 @@ import { AdminService } from './../admin.service';
   styleUrls: ['./admin-accueil.component.css']
 })
 export class AdminAccueilComponent implements OnInit, OnDestroy {
+  isUsermanagement: Boolean;
+  isMessageManagement: Boolean;
+  isStatistics: Boolean;
 
   constructor(
     private _adminService: AdminService
   ) { }
 
+  updateStates() {
+    this.isUsermanagement = this._adminService.getUserManagement();
+    this.isMessageManagement = this._adminService.getMessagesManagement();
+    this.isStatistics = this._adminService.getStatistics();
+  }
+
   manageUsers() {
     this._adminService.setUserManagement();
+    this.updateStates();
   };
 
   manageMessages() {
     this._adminService.setMessagesManagement();
+    this.updateStates();
   };
   showStatistics() {
     this._adminService.setStatistics()
+    this.updateStates();
   };
   ngOnInit() {
     this._adminService.setManagementPanel();
